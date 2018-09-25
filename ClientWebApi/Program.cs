bd1b61fcadae54964699b5f85b12d0e4c253d0bf
capfitech <capfitech@CAPFIDESKTOP104>
2018-09-25 14:23:48 +0200

    Ajoutez des fichiers projet.

---
+++ ClientWebApi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientWebApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var client = new HttpClient();
+            var result =  client.GetAsync("http://localhost:59280/api/helpdesk/test").Result;
+            var applications = result.Content.ReadAsAsync<List<string>>().Result;
+            foreach (var item in applications)
+            {
+                Console.WriteLine(item);
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
